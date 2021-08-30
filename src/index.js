6a951e07154722fb23ece22ce6f9ecca3b3ab79b
Kumar Prafull <eprafull@gmail.com>
2021-08-30 14:37:20 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './App'
+import 'tachyons';
+
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
